14f9130c87451b7dfd9fba690cb2e0b896faf4d5
lucki90 <tomlucki@wp.pl>
2023-04-26 18:13:24 +0200

    T1 - pierwsze sugestie do poprawy

--- src/samouczekJavaSklepInternetowy/Sklep.java
+++ src/samouczekJavaSklepInternetowy/Sklep.java
@@ -4,27 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Sklep {
-
-    Map<Integer, Przedmiot> map = new HashMap<>();
-
-
+        //to powinno byc private
+    Map<Integer, Przedmiot> map = new HashMap<>(); // zmiena nazwe. nie map tylko //mapaTowarowWSklepie
 
     public void przygotujTowary() {
-
         map.put(0, new Przedmiot("lopata", 10));
         map.put(1, new Przedmiot("grabie", 5));
         map.put(2, new Przedmiot("ziemia", 3));
         map.put(3, new Przedmiot("donica", 35));
-
-
     }
 
     public void wyswietlTowary() {
         for (Map.Entry<Integer, Przedmiot> wyswietlTowarDostepnywSklepie : map.entrySet()) {
-            System.out.println(wyswietlTowarDostepnywSklepie);
+            System.out.println(wyswietlTowarDostepnywSklepie); // zmien nazwe na towarWSklepie
         }
-
-
     }
 
     public void setMap(Map<Integer, Przedmiot> map) {
